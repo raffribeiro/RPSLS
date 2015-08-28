3515f18852268a450d667ebe89ab5fb61dcb38a7
isgustavo <himynameisgustavo@gmail.com>
2015-08-28 11:10:23 -0300

    Initial Commit

---
+++ RPSLS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RPSLS
+//
+//  Created by gustavo.freitas2 on 8/28/15.
+//  Copyright (c) 2015 himynameisgustavo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
